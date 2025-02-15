6d3c3b0f81ed81727d3b9fa1213bed000ddfa469
Maik Jablonski <jablonski@neoskop.de>
2025-02-15 12:21:33 +0100

    upstream

--- src/routes/client-jsx/[index].jsx
+++ src/routes/client-jsx/[index].jsx
@@ -2,8 +2,11 @@ import Layout from "../Layout";
 
 export default function () {
   return (
-    <Layout script="/client-jsx/index.js">
-      <h1 class="center">JSX components in the browser</h1>
+    <Layout
+      title="Asynchronous JSX components in the browser"
+      script="/client-jsx/index.js"
+    >
+      <h1 class="center">(Asynchronous) JSX components in the browser</h1>
       <p class="center">
         You can use JSX components built with{" "}
         <a href="https://github.com/jeasx/jsx-async-runtime" target="_blank">
@@ -12,31 +15,33 @@ export default function () {
         also in the browser for lightweight rendering of client-side markup.
       </p>
       <form
-        action={
-          /* js */ `javascript:
+        onsubmit={
+          /* js */ `
+          event.preventDefault();
+          const form = new FormData(event.target);
           renderJSX(
-            "root", 
-            document.getElementById('component').value,
+            document.getElementById("jokes"), 
+            form.get("component"),
             {
-                category: document.getElementById('category').value,
-                amount: document.getElementById('number').value
+                category: form.get("category"),
+                amount: form.get("amount")
             }
             );`
         }
       >
-        <input type="number" id="number" min="1" max="5" value="3" />
-        <select id="category">
+        <input name="amount" type="number" min="1" max="5" value="3" />
+        <select name="category">
           <option>general</option>
           <option>knock-knock</option>
           <option>programming</option>
         </select>
-        <select id="component">
+        <select name="component">
           <option value="JokesHTML">HTML</option>
           <option value="JokesJSON">JSON</option>
         </select>
         <button type="submit">Tell me jokes</button>
       </form>
-      <div id="root"></div>
+      <div id="jokes"></div>
     </Layout>
   );
 }
