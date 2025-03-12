b5209443c3a9f8615d356dc1aa9126b7b7aa5901
Maik Jablonski <jablonski@neoskop.de>
2025-03-12 08:37:34 +0100

    removed test

--- src/browser/client-jsx/components/JokesHTML.jsx
+++ src/browser/client-jsx/components/JokesHTML.jsx
@@ -4,7 +4,6 @@ export default async function JokesHTML({ category, amount }) {
   const jokes = await fetchJokes(category, amount);
   return (
     <ul>
-      <p>${process.env.BROWSER_PUBLIC_BUILD_TIME}</p>
       {jokes.map((joke) => (
         <li>{joke}</li>
       ))}
