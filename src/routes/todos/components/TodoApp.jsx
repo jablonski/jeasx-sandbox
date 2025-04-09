2ed73b591cee43d651d52bfcbf28bb76038e1ce5
Maik Jablonski <jablonski@neoskop.de>
2025-04-09 17:23:14 +0200

    rev

--- src/routes/todos/components/TodoApp.jsx
+++ src/routes/todos/components/TodoApp.jsx
@@ -12,7 +12,7 @@ export default function TodoApp({ todos, status, autofocus }) {
     >
       <section class="todoapp">
         <header class="header">
-          <h1>my todos</h1>
+          <h1>todos</h1>
           <TodoInput autofocus={autofocus} />
         </header>
         <section class="main">
