d08cf5e42b4b3d7fb247427f2846f459134e6e10
Maik Jablonski <jablonski@neoskop.de>
2025-12-01 00:37:18 +0100

    cleanup .env.js

--- .env.js
+++ .env.js
@@ -5,7 +5,7 @@ export default {
   FASTIFY_SERVER_OPTIONS: {
     disableRequestLogging: NODE_ENV_IS_DEVELOPMENT,
     bodyLimit: 1024 * 1024,
-    rewriteUrl: (req) => String(req.url).replace(/^\/foo/, ""),
+    rewriteUrl: (req) => String(req.url).replace(/^\/jeasx/, ""),
     // @ts-ignore
     http2: !NODE_ENV_IS_DEVELOPMENT,
   },
@@ -20,15 +20,15 @@ export default {
     },
   },
 
-  /** @type import("@fastify/formbody").FastifyFormbodyOptions */
-  // FASTIFY_FORMBODY_OPTIONS: {},
-
-  /** @type import("@fastify/multipart").FastifyMultipartOptions */
-  // FASTIFY_MULTIPART_OPTIONS: {},
-
   /** @type import("@fastify/static").FastifyStaticOptions */
   FASTIFY_STATIC_OPTIONS: {
     immutable: !NODE_ENV_IS_DEVELOPMENT,
     maxAge: NODE_ENV_IS_DEVELOPMENT ? "0" : "365d",
   },
+
+  /** @type import("@fastify/formbody").FastifyFormbodyOptions */
+  // FASTIFY_FORMBODY_OPTIONS: {},
+
+  /** @type import("@fastify/multipart").FastifyMultipartOptions */
+  // FASTIFY_MULTIPART_OPTIONS: {},
 };
