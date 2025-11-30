d8eff5340c94ba7547201134cb5554574fb7a2cb
Maik Jablonski <jablonski@neoskop.de>
2025-11-30 18:17:30 +0100

    .env.js: NODE_ENV_IS_DEVELOPMENT

--- .env.js
+++ .env.js
@@ -1,11 +1,13 @@
+const NODE_ENV_IS_DEVELOPMENT = process.env.NODE_ENV === "development";
+
 export default {
   /** @type import("fastify").FastifyServerOptions */
   FASTIFY_SERVER_OPTIONS: {
-    disableRequestLogging: process.env.NODE_ENV === "development",
+    disableRequestLogging: NODE_ENV_IS_DEVELOPMENT,
     bodyLimit: 1024 * 1024,
     rewriteUrl: (req) => String(req.url).replace(/^\/foo/, ""),
     // @ts-ignore
-    http2: process.env.NODE_ENV !== "development",
+    http2: !NODE_ENV_IS_DEVELOPMENT,
   },
 
   /** @type import("@fastify/cookie").FastifyCookieOptions */
@@ -26,7 +28,7 @@ export default {
 
   /** @type import("@fastify/static").FastifyStaticOptions */
   FASTIFY_STATIC_OPTIONS: {
-    immutable: process.env.NODE_ENV !== "development",
-    maxAge: process.env.NODE_ENV === "development" ? "0" : "365d",
+    immutable: !NODE_ENV_IS_DEVELOPMENT,
+    maxAge: NODE_ENV_IS_DEVELOPMENT ? "0" : "365d",
   },
 };
